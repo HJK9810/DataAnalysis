6327c70d02750d8c2f534173e0e9f954e3663995
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-08-09 08:55:20 +0900

    modify all data write

--- src/rtdownAnalys/ReadCSV.java
+++ src/rtdownAnalys/ReadCSV.java
@@ -9,127 +9,116 @@ import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class ReadCSV {
-	private static String DIRECTION = "C:\\javatest\\dataAnalysis\\ApartDeal\\";
-	private static String SUJIFILE = "아파트(매매)__실거래가_20220808095913.csv";
-	private static String SEOUL = "아파트(매매)__실거래가_20220808154349.csv";
-	private static HashMap<String, List<String>> landDatas = new HashMap<String, List<String>>();
-	private static HashSet<Integer> title = new HashSet<>();
-	
-	protected void DataInput() {
-//		HashMap<String, List<String>> landDatas = new HashMap<String, List<String>>();
-		File f = new File(DIRECTION + SEOUL);
-		try {
-			CSVReader read = new CSVReader(new FileReader(f));
-			String[] readLine;
-			int count = 0;
-			List<String> datas = new ArrayList<>();
+    private static String DIRECTION = "C:\\javatest\\dataAnalysis\\Apt";
+    private static HashMap<String, List<String>> landDatas = new HashMap<String, List<String>>();
+    private static HashSet<Integer> title = new HashSet<>();
 
-			while ((readLine = read.readNext()) != null) {
-				count++;
-				if (count < 17) continue;
-				
-				String keys = readLine[0] + "," + readLine[4] + "," + Math.floor(Double.parseDouble(readLine[5]));
-				if(!landDatas.containsKey(keys)) datas = new ArrayList<>();
-				else datas = landDatas.get(keys);
+    public List getFileList() {
+        File myDir = new File(DIRECTION);
+        File[] contents = myDir.listFiles();
+        List<File> fileList = new ArrayList<>();
+        for (File temp : contents) {
+            fileList.add(temp);
+        }
+        return fileList;
+    }
 
-				String price = readLine[8].replace(",", "");
-				title.add(Integer.parseInt(readLine[6]));
-				datas.add(readLine[6] + "," + price);
-//				System.out.println(readLine[4] + ", " + readLine[5]);
-				landDatas.put(keys, datas);
-			}
-		} catch (IOException | CsvValidationException e) {
-			e.printStackTrace();
-		}
-		System.out.println();
-	}
-	
-	protected void Datawrite() {
-		File f = new File("C:\\javatest\\dataAnalysis\\Analysis\\월별서울아파트매매가.csv");
-		try {
-			CSVWriter write = new CSVWriter(new FileWriter(f));
-			List<Integer> years = new ArrayList<Integer>(title);
-			Collections.sort(years);
-			List<String> indexTitle = new ArrayList<String>();
-			
-			indexTitle.add("시군구");
-			indexTitle.add("단지명");
-			indexTitle.add("전용면적(㎡)");
-			for(int year : years) {
-				indexTitle.add(Integer.toString(year));
-			}
-			
-			write.writeNext(indexTitle.toArray(new String[indexTitle.size()]));
-			
-			for (Entry<String, List<String>> entry : landDatas.entrySet()) {
-				String[] perDates = new String[15];
-				List<String> data = entry.getValue();
-				perDates[0] = entry.getKey().split(",")[0];
-				perDates[1] = entry.getKey().split(",")[1];
-				perDates[2] = entry.getKey().split(",")[2];
-				String date = "";
-				double sum = 0;
-				int count = 0;
-				int idx = 0;
+    protected void DataInput(String fileName) {
+        try {
+            System.out.println(fileName);
+            CSVReader read = new CSVReader(new InputStreamReader(new FileInputStream(fileName), "CP949"));
+            String[] readLine;
+            int count = 0;
+            List<String> datas = new ArrayList<>();
 
-				for(String line : data) {
-					String[] ary = line.split(",");
-					if(date.isEmpty()) date = ary[0];
-					
-			   		if(!date.equals(ary[0])) {
-			   			idx = indexTitle.indexOf(date);
-			   			
-			   			if(sum != 0 && count != 0) perDates[idx] = String.format("%.2f", sum / count);
-			   			date = ary[0];
-			   			sum = Integer.parseInt(ary[1].replace(",", ""));
-			   			count = 1;
-			   		} else {
-			   			sum += Integer.parseInt(ary[1].replace(",", ""));
-			   			count++;
-			   		}
-				}
-				
-				idx = indexTitle.indexOf(date);
-				perDates[idx] = String.format("%.2f", sum / count);
-				write.writeNext(perDates);
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	protected void FileReadAll() {
-		File f = new File(DIRECTION + SUJIFILE);
-		try {
-			CSVReader read = new CSVReader(new FileReader(f));
-			String[] readLine;
-			int count = 0;
+            while ((readLine = read.readNext()) != null) {
+                count++;
+                if (count < 17) continue;
 
-			while ((readLine = read.readNext()) != null) {
-				count++;
-				if (count < 16) continue;
-				System.out.printf("%-20s %8s %6s %8s %3s\n", readLine[4], readLine[5], readLine[6], readLine[8], readLine[9]);
-			}
-		} catch (IOException | CsvValidationException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	protected void FileReadPerName() {
-		for (Entry<String, List<String>> entry : landDatas.entrySet()) {
-			List<String> data = entry.getValue();
-			System.out.println("======================================");
-			System.out.printf("%s : 전체 %d 건 거래\n", entry.getKey(), data.size());
-			System.out.println("======================================");
-			for(String line : data) {
-				String[] ary = line.split(",");
-		   		System.out.println("계약년월 : " + ary[0]);
-		   		System.out.println("거래금액(만원) : " + ary[1]);
-		   		System.out.println("--------------------------------------");
-			}
-			
-	   		System.out.println("======================================");
-		}
-	}
+                if(!readLine[12].equals("")) continue;
+
+                double floorSize = 0;
+                String keys;
+                int addN = 0;
+                try {
+                    floorSize = Double.parseDouble(readLine[5]);
+                    keys = readLine[0] + "\t" + readLine[4] + "\t" + Math.floor(floorSize);
+                } catch (NumberFormatException  e) {
+                    keys = readLine[0] + "\t" + readLine[4]+","+ readLine[5] + "\t" + Math.floor(Double.parseDouble(readLine[6]));
+                    addN = 1;
+                }
+
+                if (!landDatas.containsKey(keys)) datas = new ArrayList<>();
+                else datas = landDatas.get(keys);
+
+                String price = readLine[8 + addN].replace(",", "");
+                title.add(Integer.parseInt(readLine[6]));
+                datas.add(readLine[6 + addN] + "," + price);
+                landDatas.put(keys, datas);
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+    }
+
+    protected void Datawrite() {
+        HashMap<String, List<String>> check = landDatas;
+        HashSet<Integer> check2 = title;
+        File f = new File("C:\\javatest\\dataAnalysis\\Analysis\\전체월별서울아파트매매가.csv");
+        try {
+            CSVWriter write = new CSVWriter(new FileWriter(f));
+            List<Integer> years = new ArrayList<Integer>(title);
+            Collections.sort(years);
+            List<String> indexTitle = new ArrayList<String>();
+
+            indexTitle.add("시군구");
+            indexTitle.add("단지명");
+            indexTitle.add("전용면적(㎡)");
+            for (int year : years) {
+                indexTitle.add(Integer.toString(year));
+            }
+
+            write.writeNext(indexTitle.toArray(new String[indexTitle.size()]));
+
+            for (Entry<String, List<String>> entry : landDatas.entrySet()) {
+                String[] perDates = new String[indexTitle.size()];
+                List<String> data = entry.getValue();
+                perDates[0] = entry.getKey().split("\t")[0];
+                perDates[1] = entry.getKey().split("\t")[1];
+                perDates[2] = entry.getKey().split("\t")[2];
+                String date = "";
+                double sum = 0;
+                int count = 0;
+                int idx = 0;
+
+                for (String line : data) {
+                    String[] ary = line.split(",");
+                    if (date.isEmpty()) date = ary[0];
+
+                    if (!date.equals(ary[0])) {
+                        idx = indexTitle.indexOf(date);
+
+                        if (sum != 0 && count != 0) perDates[idx] = String.format("%.2f", sum / count);
+                        date = ary[0];
+                        sum = Integer.parseInt(ary[1].replace(",", ""));
+                        count = 1;
+                    } else {
+                        sum += Integer.parseInt(ary[1].replace(",", ""));
+                        count++;
+                    }
+                }
+
+                idx = indexTitle.indexOf(date);
+                perDates[idx] = String.format("%.2f", sum / count);
+                write.writeNext(perDates);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        landDatas.clear();
+        title.clear();
+    }
 }
--- src/rtdownAnalys/RealEstate.java
+++ src/rtdownAnalys/RealEstate.java
@@ -1,21 +1,18 @@
 package rtdownAnalys;
 
-import java.util.Scanner;
+import java.util.List;
 
 public class RealEstate {
 
 	public static void main(String[] args) {
 		ReadCSV rc = new ReadCSV();
-		Scanner input = new Scanner(System.in);
-		rc.DataInput();
+
+		List fileList = rc.getFileList();
+		for(int i =0;i<fileList.size();i++) {
+			System.out.println("No. "+ i + " file start");
+			rc.DataInput(fileList.get(i).toString());
+			System.out.println("No. "+ i + " file clear");
+		}
 		rc.Datawrite();
-		
-//		System.out.println("확인하고자 하는 데이터를 입력해주세요.");
-//		System.out.println(" 1. 전체 데이터 출력");
-//		System.out.println(" 2. 단지별 데이터 출력");
-//		int num = input.nextInt();
-//		
-//		if(num == 1) rc.FileReadAll();
-//		else if(num == 2) rc.FileReadPerName();
 	}
 }
